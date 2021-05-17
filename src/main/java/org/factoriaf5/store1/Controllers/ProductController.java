2ac95736b8b6900d1d703ec5821ad7dbc611d952
superco-art <daniela.trifu@gmail.com>
2021-05-17 12:18:15 +0200

    add system properties

--- src/main/java/org/factoriaf5/store1/Controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store1/Controllers/ProductController.java
@@ -1,4 +1,15 @@
 package org.factoriaf5.store1.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class ProductController {
+
+
+    @GetMapping("/health")
+    public String check() {
+        return "Hello! I'm a server.";
+    }
 }
