50f849dfcb86650034d57f251699e4d7b5f63113
superco-art <daniela.trifu@gmail.com>
2021-05-17 15:19:32 +0200

    add products

--- src/main/java/org/factoriaf5/store1/Controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store1/Controllers/ProductController.java
@@ -1,15 +1,30 @@
 package org.factoriaf5.store1.Controllers;
 
+import org.factoriaf5.store1.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
 public class ProductController {
 
+    private List<Product> products;
+    public ProductController(){
 
-    @GetMapping("/health")
-    public String check() {
-        return "Hello! I'm a server.";
+        products = List.of(
+                new Product(20,"Lotion SPF"),
+                new Product(15, "Lotion After Sun")
+                );
+    }
+
+    private List<Product> products = new ArrayList<>();
+
+    @GetMapping("/products")
+    public List<Product> check() {
+        public List<Product> allProducts()
+        return products;
     }
 }
