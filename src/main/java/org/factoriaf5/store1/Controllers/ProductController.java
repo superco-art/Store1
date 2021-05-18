5bed8694aff694209d7fc07524b18cf5b944ed18
superco-art <daniela.trifu@gmail.com>
2021-05-18 15:34:33 +0200

    addding deletemapping

--- src/main/java/org/factoriaf5/store1/Controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store1/Controllers/ProductController.java
@@ -1,6 +1,7 @@
 package org.factoriaf5.store1.Controllers;
 
 import org.factoriaf5.store1.model.Product;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +30,8 @@ public class ProductController {
         public void addProducts(Product product) {
         products.add(product);
         }
+
+        @DeleteMapping("/products")
+
+
     }
